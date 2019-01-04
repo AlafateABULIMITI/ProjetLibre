be8e474cdc0dcab18985c9a85340a3345f9301e2
“AlafateABULIMITI” <felixneoaa@gmail.com>
2019-01-04 18:20:34 +0100

    清理quantity这一列，但是还有问题，"300（）"这个值清不掉，有bug

--- analysis.py
+++ analysis.py
@@ -3,19 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Levenshtein as ls
 
-file = 'en.openfoodfacts.org.products.csv'
-df = pd.DataFrame()
-with open(file)as f:
-    chunk_iter = pd.read_csv(file, sep='\t', iterator=True, chunksize=100000)
-    for chunk in chunk_iter:
-        df = pd.concat([df, chunk])
 
-cols = df.columns.values.tolist()
-
-print(cols)
-
-
-def separateData(dataset):
+def separateData(data):
     pass
 
 
@@ -34,3 +23,45 @@ def levenshteinDistance(data1, data2):
 def calculateDis(data1, data2):
     dis = euclideanDistance(data1, data2) + levenshteinDistance(data1, data2)
     return dis
+
+
+def cleanData(df):
+    # 1.quantitiy
+    df.rename(columns={'quantity': 'quantity(g)'}, inplace=True)
+    df['quantity(g)'].fillna('0', inplace=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('grammes', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('kg', '000', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('g', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace(' ', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('Vrac', '0', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('ml', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('capsules', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('l', '000', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace(
+        '1.008000/12pain', '0', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('c000e', '000', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace(',', '.', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('4x5c000', '0', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('1bun', '0', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('e', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('r', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('c000', '000', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('(nto)', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].replace('()', '', regex=True)
+    df['quantity(g)'] = df['quantity(g)'].astype(float)
+
+
+
+
+if __name__ == "__main__":
+    file = 'en.openfoodfacts.org.products.csv'
+    df = pd.DataFrame()
+    with open(file)as f:
+        chunk_iter = pd.read_csv(file, sep='\t', iterator=True, chunksize=100000)
+        for chunk in chunk_iter:
+            df = pd.concat([df, chunk])
+
+    cols = df.columns.values.tolist()
+    df.info()
+    print(cols)
+
