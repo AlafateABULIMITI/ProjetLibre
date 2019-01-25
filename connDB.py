2bbe037c4598c6b03be956329762fa34da8e7cf1
“AlafateABULIMITI” <felixneoaa@gmail.com>
2019-01-25 13:26:07 +0100

    connDBd

--- connDB.py
+++ connDB.py
@@ -1,15 +1,28 @@
 from pymongo import MongoClient
+import pandas as pd
 
-def connecDB():
-    conn = MongoClient('mongodb://localhost:27017/')
-    dblist =conn.list_database_names()
-    if 'openFood'in dblist:
-        myDB=conn["openFood"]
-        collist =myDB .list_collection_names()
-        if 'openFood' in collist:
-            print("Collection openFood exists")
-            return myDB["openFood"]
+class ConnDB:
+
+    def connect_mongo(host, port, username, password, db):
+        """ A util for making a connection to mongo """
+        if username and password:
+            mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+            conn = MongoClient(mongo_uri)
         else:
-            print(' Collection openFood no exists')
-    else:
-        print(' DB openFood no exists')
+            conn = MongoClient(host, port)
+
+        return conn[db]
+
+    def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+        """ Read from Mongo and Store into DataFrame """
+
+        # Connect to MongoDB
+        db = db.connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+        # Make a query to the specific DB and Collection
+        cursor = db[collection].find(query)
+
+        # Expand the cursor and construct the DataFrame
+        df = pd.DataFrame(list(cursor))
+
+        return df
